6e3f527d527de735dc60dd7a6840bd8149e8b64b
bilallh <bilallawalhassan.com>
2022-05-21 12:36:12 +0300

    javascript

---
+++ main.js
@@ -0,0 +1,455 @@
+/*let age=30;
+age=31;
+console.log('her age is ' +age)
+const age=30;
+console.log('her age is' +age);
+
+const age=21;
+console.log(age);
+const name= 'john';
+const age=30;
+const male=true;
+const female=false;
+const school=undefined;
+let z;
+console.log(typeof name);
+ const age=30;
+ const name='name';
+
+
+template string
+const hello=`my name is ${name} and iam ${age}`;
+console.log(hello);
+const gender='male';
+const status='singe';
+let age=30;
+age=35;
+console.log(`my gender is ${gender} my status ${status} my age is ${age}`);
+
+const name='name';
+const age='age';
+console.log(`my name is $ {name} my age is${age}`);
+
+const s='hello world';
+
+console.log(s.length);
+
+
+const j='man'
+console.log(j.toLocaleUpperCase());
+
+const f='mncguc';
+console.log(f.split(''));
+
+const h='technology,computer,science'
+console.log(h.split(','));
+
+
+const fruits=['apples','graves','oranges','mangoes'];
+fruits[4]='strawberries';
+console.log(fruits);
+ 
+const names=['bilal','lawal','hassan']
+names.shift=('sa');
+names.unshift('hdgs')
+names.pop('hassan');
+console.log(names);
+let age=30;
+age=31;
+console.log('her age is ' +age)
+const age=30;
+console.log('her age is' +age);
+
+const age=21;
+console.log(age);
+const name= 'john';
+const age=30;
+const male=true;
+const female=false;
+const school=undefined;
+let z;
+console.log(typeof name);
+ const age=30;
+ const name='name';
+
+template string
+const hello=`my name is ${name} and iam ${age}`;
+console.log(hello);
+const gender='male';
+const status='singe';
+let age=30;
+age=35;
+console.log(`my gender is ${gender} my status ${status} my age is ${age}`);
+
+const name='name';
+const age='age';
+console.log(`my name is $ {name} my age is${age}`);
+
+const s='hello world';
+
+console.log(s.length);
+
+
+const j='man'
+console.log(j.toLocaleUpperCase());
+
+const f='mncguc';
+console.log(f.split(''));
+
+const h='technology,computer,science'
+console.log(h.split(','));
+
+
+const fruits=['apples','graves','oranges','mangoes'];
+fruits[4]='strawberries';
+console.log(fruits);
+ 
+const names=['bilal','lawal','hassan']
+names.shift=('sa');
+names.unshift('hdgs')
+names.pop('hassan');
+console.log(names);
+let age=30;
+age=31;
+console.log('her age is ' +age)
+const age=30;
+console.log('her age is' +age);
+
+const age=21;
+console.log(age);
+const name= 'john';
+const age=30;
+const male=true;
+const female=false;
+const school=undefined;
+let z;
+console.log(typeof name);
+ const age=30;
+ const name='name';
+
+template string
+const hello=`my name is ${name} and iam ${age}`;
+console.log(hello);
+const gender='male';
+const status='singe';
+let age=30;
+age=35;
+console.log(`my gender is ${gender} my status ${status} my age is ${age}`);
+
+const name='name';
+const age='age';
+console.log(`my name is $ {name} my age is${age}`);
+
+const s='hello world';
+
+console.log(s.length);
+
+
+const j='man'
+console.log(j.toLocaleUpperCase());
+
+const f='mncguc';
+console.log(f.split(''));
+
+const h='technology,computer,science'
+console.log(h.split(','));
+
+
+const fruits=['apples','graves','oranges','mangoes'];
+fruits[4]='strawberries';
+console.log(fruits);
+ 
+const names=['bilal','lawal','hassan']
+names.shift=('sa');
+names.unshift('hdgs')
+names.pop('hassan');
+console.log(names);
+
+const data=['names','surname','address','number'];
+data.shift=('gender');
+data.unshift=('male');
+data.pop();
+console.log(data);
+console.log(Array.isArray(data));
+const person={
+    name:'bilal',
+    surname:'lawal',
+hobbies:['running','jumping','swimming'],
+address:{
+    state:'katsina',
+    area:'layout',
+
+}
+}
+console.log(person)
+
+
+const data=['names','surname','address','number'];
+data.shift=('gender');
+data.unshift=('male');
+data.pop();
+console.log(data);
+console.log(Array.isArray(data));
+const person={
+    name:'bilal',
+    surname:'lawal',
+hobbies:['running','jumping','swimming'],
+address:{
+    state:'katsina',
+    area:'layout',
+
+}
+}
+ console.log(person)
+
+
+const data = ['names', 'surname', 'address', 'number'];
+data.shift = ('gender');
+data.unshift = ('male');
+data.pop();
+console.log(data);
+console.log(Array.isArray(data));
+const person = {
+    name: 'bilal',
+    surname: 'lawal',
+    hobbies: ['running', 'jumping', 'swimming'],
+    address: {
+        state: 'katsina',
+        area: 'layout',
+
+    }
+}
+console.log(person);
+
+const bilal={
+    name:'bilal',
+    surname:'lawal',
+    hpbbies:['reading,running,dancing'],
+    address:{
+        state:'katsina',
+        area:'layout',
+        number:0709999,
+    }
+}
+
+console.log(bilal.hpbbies);
+bilal.emial='bilal@gmail.com';
+console.log(bilal);
+
+const todo = [
+    { id:1,
+        text:'take out trash',
+        iscompleted:'true'
+
+
+    },
+    { id:2,
+        text:'meeting with boss',
+        iscompleted:'true'
+
+
+    },
+    { id:3,
+        text:'dentist appt',
+        iscompleted:'false'
+
+
+    }
+]
+console.log(todo[2].id)  
+
+for(let i=0; i<10;i++){
+    console.log(`the number is:${i}`)
+}
+let z=0;
+while(z <= 10) {
+    console.log(`while loop number:${z}`)
+    z++;
+    
+}
+
+for(let i=0; i<10; i++){
+
+console.log(i);
+}
+
+for(let i=0; i<10; i++){
+
+
+console.log(`the number sj:${i}`);}
+
+for( let i=0;i<10; i++){
+    console.log(`the:${i}`)
+
+}
+
+for(let r of todo){
+    console.log(todo)
+}
+for (let f of todo){
+    console.log(todo)
+}
+const h=19;
+if(h<10){
+    console.log('no')
+}
+else if(h>10){
+    console.log('yes')
+}
+else{
+    console.log('y')
+}
+ 
+const i=19;
+const y=10;
+if(i==19 ||y==10){
+    console.log('yes')
+
+}
+const c=10;
+const color= c<10 ? 'water':'fire';
+console.log(color);
+
+ 
+
+const j=10;
+const nkdr=j<8 ? 'blue': 'red';
+console.log(nkdr);
+
+const x=10;
+const n=11;
+const nuber = x==10 && n==11 ? 'purpul':'white';
+console.log(nuber);
+
+const t=23;
+const clr= t<23 ?'red':'white';
+switch(clr){
+    case'red':
+    console.log('color is red')
+    break;
+     case 'blue':
+         console.log('the color is blue');
+         break;
+}
+function addnum(num1,num2){
+    console.log(num1-num2);
+}
+addnum(3,9);
+
+function Bilal(firstname,lastname,dob){
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.dob=dob;
+}
+const ahmad1=new Bilal('ahmad','lawal','20001');
+const sadiq= new Bilal('sadiq','lawal','293');
+const ibrahim=new Bilal('ibrahim','mato','7892');
+
+console.log(sadiq);
+
+function Kame(room,hjjj,hjj){
+    this.room = room;
+    this.hjjj=hjjj;
+    this.hjj=hjj;
+}
+ const Web = new Kame('bnn','nn','nnn');
+ console.log(Web);
+
+ function bal(name,surname){
+     this.name=name;
+     this.surname=surname;
+
+ }
+
+ const Na=new bal('bbb','hhh');
+ console.log(Na);
+
+ class person{
+     constructor(firstname,lastname,surname){
+         this.firstname= firstname;
+         this.lastname= lastname;
+         this.surname=surname;
+
+     }
+    
+ }
+
+ const per= new person('bilal','lawal','hassan');
+ console.log(per);
+
+ class ha{
+     constructor(firstname,lastname,surname){
+         this.firstname=firstname;
+         this.lastname=lastname;
+         this.surname=surname;
+
+     }
+
+ }
+ const ki=new ha('ddd','fff','fff');
+ console.log(ki);
+ 
+
+ console.log(window);
+ alert(1);
+console.log(document.getElementById('my-form'));
+
+console.log(document.querySelector('h1'));
+
+console.log(document.querySelectorAll('.item'));
+
+const ul=document.querySelector('.items');
+ul.remove();
+
+const ui=document.querySelector('.items');
+ui.lastElementChild.remove();
+
+const ju=document.querySelector('.items');
+ju.lastElementChild.textContent='hello';
+ju.firstElementChild.innerHTML='<h1>biai<h1/>';
+ju.children[1].innertext='hcjv';
+
+const btn=document.querySelector('.btn');
+btn.style.background='red';
+
+const btn=document.querySelector('.btn');
+btn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    console.log(e.target.class
+        
+    );
+});
+
+
+const btn=document.querySelector('.btn');
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+  document.querySelector('#my-form').style.background='red';
+} );
+
+
+const btn=document.querySelector('.btn');
+    btn.addEventListener('click',(e) =>{
+        e.preventDefault();
+        document.querySelector('body').classList.add('bg-dark')
+        document.querySelector('.items').lastElementChild.innerHTML='<h1>Bilal</h1>';
+    })
+    const form =document.querySelector('#my-form');
+    const nameInput = document.querySelector('#name');
+    const EmailInput= document.querySelector('#email');
+    const msg =document.querySelector('#msg');
+    const userlist =document.querySelector('#userlist');
+
+    form.addEventListener('submit',onSubmit);
+    function onSubmit(e){
+        e.preventDefault();
+        console.log(nameInput.value);
+
+        if(nameInput.value === '' || EmailInput.value === '') {
+            alert('please fill feild');
+        } else {
+            console.log('success');
+        }
+
+    }*/
---
+++ pract.js
@@ -0,0 +1,74 @@
+/*let age=30;
+age=31;
+console.log('her age is ' +age)*/
+/*const age=30;
+console.log('her age is' +age);
+
+const age=21;
+console.log(age);*/
+/*const name= 'john';
+const age=30;
+const male=true;
+const female=false;
+const school=undefined;
+let z;
+console.log(typeof name);
+ const age=30;
+ const name='name';*/
+
+/*
+template string
+const hello=`my name is ${name} and iam ${age}`;
+console.log(hello);
+const gender='male';
+const status='singe';
+let age=30;
+age=35;
+console.log(`my gender is ${gender} my status ${status} my age is ${age}`);
+
+const name='name';
+const age='age';
+console.log(`my name is $ {name} my age is${age}`);
+
+const s='hello world';
+
+console.log(s.length);
+
+
+const j='man'
+console.log(j.toLocaleUpperCase());
+
+const f='mncguc';
+console.log(f.split(''));
+
+const h='technology,computer,science'
+console.log(h.split(','));
+
+
+const fruits=['apples','graves','oranges','mangoes'];
+fruits[4]='strawberries';
+console.log(fruits);
+ 
+const names=['bilal','lawal','hassan']
+names.shift=('sa');
+names.unshift('hdgs')
+names.pop('hassan');
+console.log(names);
+
+const data=['names','surname','address','number'];
+data.shift=('gender');
+data.unshift=('male');
+data.pop();
+console.log(data);
+console.log(Array.isArray(data));
+const person={
+    name:'bilal',
+    surname:'lawal',
+hobbies:['running','jumping','swimming'],
+address:{
+    state:'katsina',
+    area:'layout',
+
+}
+}
+ console.log(person)*/
